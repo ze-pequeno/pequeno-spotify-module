88f4be1c32cc6c269e01a10e91504668674d3797
Cyril Martin <cyril.martin.cm@gmail.com>
2013-02-23 22:51:39 +0100

    add travis configuration

---
+++ .travis/application.config.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+    'modules' => array(
+        'PequenoSpotifyModule',
+    ),
+    'module_listener_options' => array(
+        'config_glob_paths'    => array(
+            'config/autoload/{,*.}{global,local}.php',
+        ),
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+    ),
+    'service_manager' => array(
+        'use_defaults' => true,
+        'factories'    => array(
+        ),
+    ),
+);
