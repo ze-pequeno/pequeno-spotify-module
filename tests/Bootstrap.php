90b5f816de35545875de19862ac5f8ace2235695
Cyril Martin <cyril.martin.cm@gmail.com>
2013-02-24 01:07:34 +0100

    update tests to be run from module (without application)

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -16,12 +16,61 @@
  * and is licensed under the MIT license.
  */
 
-// try to get composer loader
+// try to get composer loader (from module or application directory)
 if (!($loader = @include __DIR__.'/../vendor/autoload.php') &&
 	!($loader = @include __DIR__.'/../../../autoload.php')) {
 
-	// throw RuntimeException indicate autoload file not found
-	throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+	// setup local directories to search ZF2 library
+	$directories = array(
+		'E:/library/zend/zendframework2/latest/library/',
+		'G:/library/zend/zendframework2/latest/library/',
+	);
+
+	// iterate search directories
+	foreach ($directories as $directory) {
+
+		// get real path
+		$directory = realpath($directory);
+
+		// check search directories exist
+		if ($directory) {
+
+			// define ZF2_PATH constant and break
+			define('ZF2_PATH', $directory);
+			break;
+		}
+	}
+
+	// check if ZF2_PATH is defined
+	if (!defined('ZF2_PATH')) {
+
+		// throw RuntimeException indicate autoload file not found
+		throw new RuntimeException('"ZF2_PATH" is not defined and vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+	}
+
+	// set include path to include ZF2
+	set_include_path(implode(PATH_SEPARATOR, array(
+		ZF2_PATH,
+		get_include_path()
+	)));
+
+	// require Zend Autoloader class
+	require 'Zend/Loader/AutoloaderFactory.php';
+	require 'Zend/Loader/StandardAutoloader.php';
+
+	// setup autoloader from factory
+	\Zend\Loader\AutoloaderFactory::factory(
+		array(
+			\Zend\Loader\AutoloaderFactory::STANDARD_AUTOLOADER => array(
+				\Zend\Loader\StandardAutoloader::AUTOREGISTER_ZF => true,
+				\Zend\Loader\StandardAutoloader::ACT_AS_FALLBACK => false,
+				\Zend\Loader\StandardAutoloader::LOAD_NS => array(
+					'PequenoSpotifyModule' => __DIR__.'/../src/PequenoSpotifyModule',
+					'PequenoSpotifyModuleTest' => __DIR__.'/../tests/PequenoSpotifyModuleTest'
+				),
+			)
+		)
+	);
 }
 
 // try to get TestConfiguration.php
