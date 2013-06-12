0fed681815f88d122800eab09b78a362c1a2d094
Cyril Martin <cyril.martin.cm@gmail.com>
2013-06-12 11:55:40 +0200

    update local build script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-composer install --prefer-source --dev & ^
-.\vendor\bin\phpunit -c .\phpunit.xml --coverage-clover .\build\logs\clover.xml --exclude-group Performance & ^
+composer update --prefer-source --dev & ^
+.\vendor\bin\phpunit -c .\phpunit.xml --coverage-clover .\build\logs\clover.xml --coverage-html .\build\coverage --exclude-group Performance & ^
 .\vendor\bin\phpunit --group=Functional & ^
-.\vendor\bin\phpcs --standard=PSR2 .\src\ .\tests\ & pause
+.\vendor\bin\phpcs -a --standard=PSR2 .\src\ .\tests\ & pause
