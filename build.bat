c876651a62bbbcf74f3515f312e3029a764286de
Cyril Martin <cyril.martin.cm@gmail.com>
2014-08-18 21:07:47 +0200

    prepare development

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 composer update --prefer-source --dev & ^
-.\vendor\bin\phpunit -c .\phpunit.xml --coverage-clover .\build\logs\clover.xml --coverage-html .\build\coverage --exclude-group Performance & ^
-.\vendor\bin\phpunit --group=Functional & ^
-.\vendor\bin\phpcs -a --standard=PSR2 .\src\ .\tests\ & pause
+.\vendor\bin\php-cs-fixer fix .\src -v --dry-run & ^
+.\vendor\bin\php-cs-fixer fix .\tests -v --dry-run & ^
+.\vendor\bin\phpunit -c .\phpunit.xml --coverage-clover .\build\logs\clover.xml --coverage-html .\build\coverage & pause
